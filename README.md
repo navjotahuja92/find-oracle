5a71025c9ed39a80ef34cb0ad95aef5da827f303
Navjot Ahuja <navjot.ahuja@oracle.com>
2017-10-17 19:40:48 +0530

    Final Package.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Simple Command line tool to find people in Oracle Org
+
+1. Install this package `npm install find-oracle -g`.
+2. Just run `find-oracle "navjot ahuja"`. Enjoy :)
---
+++ index.js
@@ -0,0 +1,66 @@
+#! /usr/bin/env node
+
+const http = require('http');
+
+var limit = 3;
+if (process.argv.indexOf("-l") != -1) { //does our flag exist?
+    limit = process.argv[process.argv.indexOf("-l") + 1]; //grab the next item
+}
+
+var COLOR = {
+    BOUNDARY: '\x1b[33m%s\x1b[0m',
+    HEADLINE: "\x1b[42m%s\x1b[0m",
+    EMPTY: "\x1b[43m%s\x1b[0m",
+    ERROR: "\x1b[41m%s\x1b[0m"
+};
+
+function prettyPrintContact(contact) {
+    console.log(COLOR.BOUNDARY, "===============================================");
+    console.log("ARIA:  ", contact.ariaKey);
+    console.log("EMAIL: ", contact.email);
+    console.log("NAME:  ", contact.name);
+    console.log("PHONE: ", contact.phone);
+    console.log("TITLE: ", contact.title);
+    console.log("LINK:  ", "http://namefinder.us.oracle.com/aria?n=" + contact.ariaKey)
+    console.log(COLOR.BOUNDARY, "===============================================");
+}
+
+function printResponse(response) {
+    response = JSON.parse(response);
+    // 
+    if (!response.data || response.data.length <= 0) {
+        console.log(COLOR.EMPTY, "No records found!!!");
+    } else {
+        console.log(COLOR.HEADLINE, "Records Found!!!");
+        for (var i = 0; i <= response.data.length - 1 && i < limit; i++) {
+            var contact = response.data[i];
+            prettyPrintContact(contact);
+        }
+    }
+}
+
+var options = {
+    hostname: 'namefinder.us.oracle.com',
+    port: 80,
+    path: '/ws/lookup?&q=' + process.argv[2],
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+};
+
+var req = http.request(options, function(res) {
+    res.setEncoding('utf8');
+    var data = '';
+    res.on('data', function(chunk) {  
+        data += chunk; 
+    });
+    res.on('end', function() {
+        printResponse(data);
+        console.log(COLOR.HEADLINE, "Package developerd by Navjot Ahuja. https://github.com/navjotahuja92");
+    });
+});
+req.on('error', function(e) {
+    console.log(COLOR.ERROR, 'Problem with request: ' + e.message);
+});
+req.end();
